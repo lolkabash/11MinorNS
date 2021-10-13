bbdebf373b27edea64c906288f702a2e9b6dae6b
Timothy <lolkabash@gmail.com>
2021-10-13 17:02:18 +0800

    Port Changs

--- Website/server.py
+++ Website/server.py
@@ -2,6 +2,15 @@
 from flask import Flask, render_template, request
 from web_modules import Form, site
 from datetime import date, datetime
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
+
 
 app = Flask(__name__)
 
@@ -38,4 +47,4 @@ def internal_server_error(e):
 
 
 # Run App
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=port)
